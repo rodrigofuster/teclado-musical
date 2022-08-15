7335a161ceb05dcd272d07ad847a0568f2cc4548
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-08-15 16:56:35 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 
-function tocaSom (idElementAudio) {
-document.querySelector(idElementAudio).play();
+function tocaSom (seletorAudio) {
+ const elemento = document.querySelector(seletorAudio);
+
+ if (elemento != null && elemento.localName === 'audio') {
+    elemento.play();
+ } else {
+    console.log("Elemento não encontrado ou seletor inválido");
+ }
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
 
@@ -19,6 +26,15 @@ while (contador < listaDeTeclas.length) {
         tocaSom(idAudio);
     }; 
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
+        if (evento.cod == "Space" || evento.code === "Enter") {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
