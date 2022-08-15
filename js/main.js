595681dfbb0036c7210a5d2f5b208ec7118f8b51
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-08-15 14:23:36 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,14 @@ function tocaSomPom () {
 document.querySelector('#som_tecla_pom').play();
 }
 
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom; 
+    contador = contador + 1;
+
+    console.log(contador);
+    
+}
